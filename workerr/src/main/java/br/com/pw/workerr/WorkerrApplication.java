376276affa5ab5caa6f6e565ecdf1f05c883069a
A0106001 <gabriel.damaso@telefonica.com>
2021-02-07 20:27:10 -0300

    criando projeto worker

---
+++ workerr/src/main/java/br/com/pw/workerr/WorkerrApplication.java
@@ -0,0 +1,13 @@
+package br.com.pw.workerr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkerrApplication.class, args);
+	}
+
+}
---
+++ workerr/src/test/java/br/com/pw/workerr/WorkerrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pw.workerr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
