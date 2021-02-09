fcfe0dc927ae32232a88699ba47ce172cb7a220e
A0106001 <gabriel.damaso@telefonica.com>
2021-02-09 18:13:58 -0300

    add configuration of eureka server

---
+++ eureka-server/src/test/java/br/com/pw/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pw.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
