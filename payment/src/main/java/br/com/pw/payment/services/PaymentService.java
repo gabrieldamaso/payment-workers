0883104fda695cdc425b3284f143989b27650387
A0106001 <gabriel.damaso@telefonica.com>
2021-02-09 15:55:56 -0300

    add RequestTemplate for requisitions to workerr api

--- payment/src/main/java/br/com/pw/payment/services/PaymentService.java
+++ payment/src/main/java/br/com/pw/payment/services/PaymentService.java
@@ -1,13 +1,34 @@
 package br.com.pw.payment.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.pw.payment.entities.Payment;
+import br.com.pw.payment.entities.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Value("${workerr.host}")
+	private String workerrHost;
+	
+	private final RestTemplate restTemplate;
+	
+	public PaymentService(final RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
 
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		
+		uriVariables.put("id", ""+workerId);
+		
+		Worker worker = restTemplate.getForObject(workerrHost + "/workers/{id}", Worker.class, uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days );
 	}
 }
