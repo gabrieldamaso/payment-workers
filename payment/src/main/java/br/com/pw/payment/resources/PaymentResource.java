bb8de388e6461f37adbb7f6cd7db0b5000a8101e
A0106001 <gabriel.damaso@telefonica.com>
2021-02-10 18:14:14 -0300

    exceptions and errors hystrix

--- payment/src/main/java/br/com/pw/payment/resources/PaymentResource.java
+++ payment/src/main/java/br/com/pw/payment/resources/PaymentResource.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.pw.payment.entities.Payment;
 import br.com.pw.payment.services.PaymentService;
 
@@ -21,10 +23,17 @@ public class PaymentResource {
 		this.paymentService = paymentService;
 	}
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days ){
 		Payment payment = paymentService.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative( Long workerId, Integer days ){
+		Payment payment = new Payment("TesteAfalhas",400.0, days);
+		return ResponseEntity.ok(payment);
+	}
+	
 	
 }
