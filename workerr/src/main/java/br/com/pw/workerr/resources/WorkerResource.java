ba2813e65fc32838c520c2e8316f35ed96544356
A0106001 <gabriel.damaso@telefonica.com>
2021-02-11 17:13:52 -0300

    implementing spring actuator for update of configs in runtime without shut off or restart application in workerr

--- workerr/src/main/java/br/com/pw/workerr/resources/WorkerResource.java
+++ workerr/src/main/java/br/com/pw/workerr/resources/WorkerResource.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import br.com.pw.workerr.entities.Worker;
 import br.com.pw.workerr.repository.WorkerRepository;
 
 @RestController
+@RefreshScope
 @RequestMapping("/workers")
 public class WorkerResource {
 	
